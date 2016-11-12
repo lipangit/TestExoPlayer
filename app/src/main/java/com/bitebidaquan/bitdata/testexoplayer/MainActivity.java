50b1951c80d4bf33d63efc89a11b14a8580f0e64
Nathen <lipan@jiecao.fm>
2016-11-12 23:42:37 +0800

    what different between the thread.sleep

--- app/src/main/java/com/bitebidaquan/bitdata/testexoplayer/MainActivity.java
+++ app/src/main/java/com/bitebidaquan/bitdata/testexoplayer/MainActivity.java
@@ -81,11 +81,11 @@ public class MainActivity extends AppCompatActivity implements DemoPlayer.Listen
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+//                        try {
+//                            Thread.sleep(1);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+//                        }
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
